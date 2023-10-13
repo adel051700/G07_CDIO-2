2bdcae2c7a32dd5c22eac759c40dc5dcfd52628e
Elias Larsen <elias@takefive.dk>
2023-10-13 10:40:50 +0200

    Renamed tiles to tile.jaava

---
+++ Tile.java
@@ -0,0 +1,13 @@
+class Tile {
+    private String name;
+    private String story;
+    private int value;
+    
+    public Tile(String name, String story, int value)
+    {
+        this.name = name;
+        this.story = story;
+        this.value = value;
+    }
+
+}
