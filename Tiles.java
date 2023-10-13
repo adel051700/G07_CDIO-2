e108d082bfdb7639b2472d0f6acb6d81043755f4
Elias Larsen <elias@takefive.dk>
2023-10-13 09:11:27 +0200

    Created Tiles class and english.txt file

---
+++ Tiles.java
@@ -0,0 +1,11 @@
+class Tiles {
+    private String name;
+    private String story;
+    private int value;
+    
+    public Tiles(String name, value)
+    {
+        this.name = name;
+        this.value = value;
+    }
+}
