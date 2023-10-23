042c2a86d4d4b89cd344ceb74a5137a62878cd34
Elias Larsen <elias@takefive.dk>
2023-10-23 15:34:19 +0200

    Moved getTileFromFile to Tile.java and added return statement

--- Tile.java
+++ Tile.java
@@ -1,13 +1,48 @@
-class Tile {
-    private String name;
-    private String story;
-    private int value;
-    
-    public Tile(String name, String story, int value)
+import java.io.File;
+class Tile 
+{
+  private String name;
+  private String story;
+  private int value;
+  
+  public Tile(String name, String story, int value)
     {
         this.name = name;
         this.story = story;
         this.value = value;
     }
+  public Tile[] getTilesFromFile()
+  {
+    //Create Tiles from csv file of the chosen language, and create array of type Tile
+    // with all tiles
 
+    int numberOfTiles = 11;
+    Tile[]tileArr = new Tile[numberOfTiles];
+    String language = "english";
+    try
+    {
+    var fileToRead = new File(language + ".csv");
+    var Scanner = new java.util.Scanner(fileToRead);
+    int i = 0;
+      while (Scanner.hasNextLine())
+    {
+      
+      String tile = Scanner.nextLine();
+      String[] tileValues = tile.split(";");
+      String name = tileValues[0];
+      String story = tileValues[1];
+      int value = Integer.parseInt(tileValues[2]);
+      tileArr[i] = new Tile(name,story,value);
+      i++;
+    }
+    Scanner.close();
+    } 
+    catch(Exception e)
+    {
+      System.out.println(e + " Error");
+    }
+
+    return tileArr;
+
+  }
 }
