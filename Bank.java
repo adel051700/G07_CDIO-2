337a11a7dd65947726f4a070bdcbbc6045c200d7
Mads H <madshartwich@gmail.com>
2023-10-23 15:41:36 +0200

    created bank class

---
+++ Bank.java
@@ -0,0 +1,18 @@
+public class Bank{
+
+private int balance = 0;
+
+public changeBalance(int balance){
+    balance = balance + tile.value;
+    this.balance = balance;
+
+}
+
+balance getBalance(){
+    return this.balance;
+}
+
+    
+    
+}
+
