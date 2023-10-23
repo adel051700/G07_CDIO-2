b824f29d6e042be814ed80726c563d7e877571fd
Elias Larsen <elias@takefive.dk>
2023-10-23 16:07:51 +0200

    Added return statement and changed getbalance to Void

--- Bank.java
+++ Bank.java
@@ -2,13 +2,14 @@ public class Bank{
 
 private int balance = 0;
 
-public changeBalance(int balance){
-    balance = balance + tile.value;
-    this.balance = balance;
+public void changeBalance(int deltaBalance)
+{
+    this.balance += deltaBalance;
 
 }
 
-public getBalance(){
+public int getBalance()
+{
     return this.balance;
 }
 
