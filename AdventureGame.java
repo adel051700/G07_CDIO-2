12492daca06239371dd12a297696ba8d7325def9
Adel Ahmad <adel051700@gmail.com>
2023-10-24 16:22:18 +0200

    added player and diceroll and sum of dice

--- AdventureGame.java
+++ AdventureGame.java
@@ -1,7 +1,17 @@
 public class AdventureGame {
 
     public static void main(String[] args) {
+        Player player1=new Player(1);
+        Player Player2=new Player(2);
+        boolean player1turn =true;
+        Dice dice1=new Dice(6);
+        Dice dice2=new Dice(6);
         
+        int sum=dice1.rollDie() + dice2.rollDie();
+        
+        Tile[] tiles= Tile.getTilesFromFile();
+        tiles[0].getStory();
+
     }
     
 }
