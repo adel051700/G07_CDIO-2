e44d7e547010ce5960b41630b548bf68d97a5b95
Adel Ahmad <adel051700@gmail.com>
2023-10-25 09:06:13 +0200

    Added start message and win message in AdventureGame and Added message to Tile 1 even though it's impossible to land there

--- AdventureGame.java
+++ AdventureGame.java
@@ -1,23 +1,33 @@
+import java.util.Scanner;
 public class AdventureGame {
 
     public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);  
         Player player1 = new Player(1);
-        Player Player2 = new Player(2);
+        Player player2 = new Player(2);
         boolean player1turn = true;
         Dice dice1 = new Dice(6);
         Dice dice2 = new Dice(6);
 
-        System.out.println("Welcome to the game! Player 1, press enter to start your journey!");
-
+        System.out.println("Welcome to the game! Press enter to start your journey!");
+        s.nextLine();
+        
         while (player1.getBalance() < 3000 && player2.getBalance() < 3000) {
+            if (player1turn){
+            System.out.println("It's player one's turn, press enter to roll the die!");
+        } else {
+            System.out.println("It's player two's turn, press enter to roll the die!");
+        }
+        
+        s.nextLine();
             if (player1turn) {
                 int sum = dice1.rollDie() + dice2.rollDie();
                     if(sum != 10){
                         player1turn = false;
                     }
                 Tile[] tiles = Tile.getTilesFromFile();
-                System.out.println(tiles[sum].getStory());
-                player1.changeBalance(tiles[sum].getValue());
+                System.out.println(tiles[sum-1].getStory());
+                player1.changeBalance(tiles[sum-1].getValue());
                 System.out.println(player1.toString());
             } else if (!player1turn) {
                 int sum = dice1.rollDie() + dice2.rollDie();
@@ -25,11 +35,17 @@ public class AdventureGame {
                         player1turn = true;
                     }
                 Tile[] tiles = Tile.getTilesFromFile();
-                System.out.println(tiles[sum].getStory());
-                player2.changeBalance(tiles[sum].getValue());
+                System.out.println(tiles[sum-1].getStory());
+                player2.changeBalance(tiles[sum-1].getValue());
                 System.out.println(player2.toString());
             }
         }
+        if (player1.getBalance()>3000){
+            System.out.println(player1.toString()+ " which mean they win the game, congrats!");
+        } else if(player2.getBalance()>3000){
+            System.out.println(player2.toString()+ " which mean they win the game, congrats!");
+        }
+        s.close();
     }
 
 }
