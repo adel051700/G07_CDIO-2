4e483675a949bfb02a0690381dfdcf91b6c846f8
Adel Ahmad <adel051700@gmail.com>
2023-10-26 12:57:47 +0200

    better readability for devs

--- Game/Bank.java
+++ Game/Bank.java
@@ -1,26 +1,21 @@
-public class Bank{
+public class Bank {
 
-private int balance = 1000;
+    private int balance = 1000;
 
-public void changeBalance(int deltaBalance)
-{
-    this.balance += deltaBalance;
-    if (this.balance < 0)
-    {
-        this.balance = 0;
+    public void changeBalance(int deltaBalance) {
+        this.balance += deltaBalance;
+        if (this.balance < 0) {
+            this.balance = 0;
+        }
+
+    }
+
+    public int getBalance() {
+        return this.balance;
+    }
+
+    public Bank() {
+
     }
 
 }
-
-public int getBalance()
-{
-    return this.balance;
-}
-
-public Bank() 
-{
-
-}    
-    
-}
-
